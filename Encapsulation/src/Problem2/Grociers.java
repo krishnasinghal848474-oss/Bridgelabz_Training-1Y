5796a8ac127901c178ad8a2428dbb83e8dd229d5
krishnasinghal848474-oss <krishnasinghal848474@gmail.com>
2026-03-24 19:13:33 +0530

    "[krishna] files Added"

---
+++ Encapsulation/src/Problem2/Grociers.java
@@ -0,0 +1,13 @@
+package Problem2;
+class Groceries extends Product
+{
+    public Groceries(int id, String name, double price)
+    {
+        super(id, name, price);
+    }
+    public double calculateDiscount()
+    {
+        return 0;
+    }
+}
+
